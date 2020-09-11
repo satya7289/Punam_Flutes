b13cde3a1fd451b1c77dfc1b83b8222cc74845f5
satya7289 <satyaprakashaman60@gmail.com>
2020-09-11 22:40:56 +0530

    currency convert complete for product detail

--- commons/context_processors.py
+++ commons/context_processors.py
@@ -3,18 +3,12 @@ from category.models import Category
 from cart.models import Cart
 
 
-def get_ip_detail(request):
-    # Get the IP of the logged in user
-    try:              
-        user_ip = request.META.get('REMOTE_ADDR')
-    except:
-        user_ip = '0.0.0.0'
-
+def request_to_geoplugin(user_ip, base_currency='USD'):
     if user_ip!='0.0.0.0' and user_ip!='127.0.0.1':
         '''
             Get the Ip detail with country name by using `geoplugin`.
         '''
-        geoplugin_url = 'http://www.geoplugin.net/json.gp?ip=' + str(user_ip)
+        geoplugin_url = 'http://www.geoplugin.net/json.gp?ip=' + str(user_ip) + '&base_currency=' + base_currency
         geoplugin = requests.get(geoplugin_url)
 
         if geoplugin.status_code == 200:
@@ -23,7 +17,20 @@ def get_ip_detail(request):
                 'country':geoplugin.json()['geoplugin_countryName'],
                 'data':geoplugin.json(),
                 'user_ip': user_ip
-            }
+        }
+    return None
+
+def get_ip_detail(request):
+    # Get the IP of the logged in user
+    try:              
+        user_ip = request.META.get('REMOTE_ADDR')
+    except:
+        user_ip = '0.0.0.0'
+
+    ip_detail = request_to_geoplugin(request)
+    if ip_detail:
+        return ip_detail
+
     return {
         'message':'fail',
         'user_ip':user_ip
--- product/views.py
+++ product/views.py
@@ -7,6 +7,7 @@ from product.models import Product, CountryCurrency
 from category.models import Category
 from commons.ip_detect import get_ip_location
 from commons.ip_detect import get_currency_from_ip, get_ip_of_the_customer
+from commons.context_processors import get_ip_detail, request_to_geoplugin
 
 class ProductListView(View):
     template_name = 'productList.html'
@@ -31,17 +32,41 @@ class ProductDetailView(View):
     def get(self, request, *args, **kwargs):
         product_id = kwargs['product_id']
         category_id = kwargs['category_id']
-        customer_ip = get_ip_of_the_customer(request)
-        country = get_currency_from_ip(customer_ip)['country']
 
         category = Category.objects.get(id=category_id)
         product = Product.objects.get(id=product_id)
-        price = product.countrycurrency_set.filter(country=country).first()
+
+        # Get the detail of the IP
+        ip_detail = get_ip_detail(request)
+        if ip_detail['message']!='fail':
+            country = ip_detail['country']
+        else:
+            country = 'Any'
+    
+        # Get the default Any Country currency of the product.
+        default = product.countrycurrency_set.filter(country='Any').first()
+
+        # Get the Country currency of the product based on IP.
+        country_currency = product.countrycurrency_set.filter(country=country).first()
+
+        if country_currency:
+            default = country_currency
+        
+        # Request geoplugin with user ip and base currency of the country.
+        geoplugin = request_to_geoplugin(ip_detail['user_ip'], default.currency)
+
+        if geoplugin:
+            price = float(default.selling_price) * geoplugin['data']['geoplugin_currencyConverter']
+            currency = geoplugin['data']['geoplugin_currencySymbol']
+        else:
+            price = float(default.selling_price) * 73.4442 # default set USD conversion rate
+            currency = '$'
 
         context = {
             'product': product,
             'category': category,
-            'price': price
+            'price': format(price,'.2f'),
+            'currency': currency
         }
         return render(request, self.template_name, context)
 
