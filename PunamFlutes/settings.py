e3b320c90678d7a1dcd022d4e60c246bd9a94048
satya7289 <satyaprakashaman60@gmail.com>
2021-02-20 13:49:41 +0530

    fix static file issue

--- PunamFlutes/settings.py
+++ PunamFlutes/settings.py
@@ -131,12 +131,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
-# static url
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'PunamFlutes/static'),
-)
+## static url
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'PunamFlutes/static'),
+# )
 
 # media Url
 MEDIA_URL = '/media/'
@@ -144,22 +144,22 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # # AWS details
 # DEFAULT_FILE_STORAGE = config('DEFAULT_FILE_STORAGE')
-# STATICFILES_STORAGE = config('STATICFILES_STORAGE')
-# AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+STATICFILES_STORAGE = config('STATICFILES_STORAGE')
+AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
-# STATIC_URL = AWS_S3_CUSTOM_DOMAIN + 'static/'
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'PunamFlutes/static'),
-# )
+STATIC_URL = AWS_S3_CUSTOM_DOMAIN + 'static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'PunamFlutes/static'),
+)
 
-# AWS_S3_OBJECT_PARAMETERS = {
-#     'CacheControl': 'max-age=86400',
-# }
-# AWS_S3_FILE_OVERWRITE = False
-# AWS_DEFAULT_ACL = None
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
 
 # Email Setup
 SEND_EMAIL = False
