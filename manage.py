52565ce09de26ab0163a6d53d673b254f66aeb51
satya7289 <satyaprakashaman60@gmail.com>
2021-02-21 01:28:09 +0530

    update for heroku

--- manage.py
+++ manage.py
@@ -2,10 +2,13 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from decouple import config
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PunamFlutes.settings')
+    if config('DEPLOYMENT',default=False, cast=bool):
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PunamFlutes.deployment')
+    else:
+        os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PunamFlutes.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
