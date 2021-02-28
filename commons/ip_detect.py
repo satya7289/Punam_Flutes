18d0bca8f0deb8d081826a62a89d7aa3e7c680e8
satya7289 <satyaprakashaman60@gmail.com>
2021-02-28 12:02:05 +0530

    ip based currency

--- commons/ip_detect.py
+++ commons/ip_detect.py
@@ -1,11 +1,11 @@
 import requests
 
-def request_to_geoplugin(user_ip, base_currency='USD'):
-    if user_ip!='0.0.0.0' and user_ip!='127.0.0.1':
+def request_to_geoplugin(client_ip, base_currency='USD'):
+    if client_ip!='0.0.0.0' and client_ip!='127.0.0.1':
         '''
             Get the Ip detail with country name by using `geoplugin`.
         '''
-        geoplugin_url = 'http://www.geoplugin.net/json.gp?ip=' + str(user_ip) + '&base_currency=' + base_currency
+        geoplugin_url = 'http://www.geoplugin.net/json.gp?ip=' + str(client_ip) + '&base_currency=' + base_currency
         geoplugin = requests.get(geoplugin_url)
 
         if geoplugin.status_code == 200:
@@ -13,24 +13,30 @@ def request_to_geoplugin(user_ip, base_currency='USD'):
                 'message': 'success',
                 'country':geoplugin.json()['geoplugin_countryName'],
                 'data':geoplugin.json(),
-                'user_ip': user_ip
+                'user_ip': client_ip
         }
     return None
 
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
 def get_ip_detail(request):
     # Get the IP of the logged in user
-    try:              
-        user_ip = request.META.get('REMOTE_ADDR')
-    except:
-        user_ip = '0.0.0.0'
 
-    ip_detail = request_to_geoplugin(request)
+    client_ip = get_client_ip(request)
+
+    ip_detail = request_to_geoplugin(client_ip)
     if ip_detail:
         return ip_detail
 
     return {
         'message':'fail',
-        'user_ip':user_ip
+        'user_ip':client_ip
     }
 
 
