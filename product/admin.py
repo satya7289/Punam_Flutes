2b461318bb041d5cbb2c4d23d7c59df472a8f6d1
satya7289 <satyaprakashaman60@gmail.com>
2021-03-13 18:56:17 +0530

    update product admin

--- product/admin.py
+++ product/admin.py
@@ -7,13 +7,19 @@ from .models import Product, CountryCurrency, ProductImage
 class CountryCurrencyInLine(admin.TabularInline):
     model = CountryCurrency
 
+class CountryCurrencyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'country', 'currency', 'MRP', 'selling_price')
+    list_filter = ('country', 'currency')
 
 class ProductAdmin(admin.ModelAdmin):
+    search_fields = ('title', 'search_tags')
+    list_display = ('__str__', 'title', 'search_tags', 'description', 'created_at')
+    list_filter = ('category', )
     inlines = [
         CountryCurrencyInLine
     ]
 
 
 admin.site.register(Product, ProductAdmin)
-admin.site.register(CountryCurrency)
+admin.site.register(CountryCurrency, CountryCurrencyAdmin)
 admin.site.register(ProductImage)
