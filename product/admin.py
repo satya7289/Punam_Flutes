6c9ea4c03288c0eb772206674749ef7b236d4c68
satya7289 <satyaprakashaman60@gmail.com>
2021-03-16 15:30:34 +0530

    fix product description in admin panel

--- product/admin.py
+++ product/admin.py
@@ -1,8 +1,16 @@
+from django import forms
 from django.contrib import admin
+from django.utils.html import format_html
+from ckeditor.widgets import CKEditorWidget
 from .models import Product, CountryCurrency, ProductImage
 
 # Register your models here.
 
+class ProductAdminForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorWidget())
+    class Meta:
+        model = Product
+        fields = '__all__'
 
 class CountryCurrencyInLine(admin.TabularInline):
     model = CountryCurrency
@@ -13,11 +21,15 @@ class CountryCurrencyAdmin(admin.ModelAdmin):
 
 class ProductAdmin(admin.ModelAdmin):
     search_fields = ('title', 'search_tags')
-    list_display = ('__str__', 'title', 'search_tags', 'description', 'created_at')
-    list_filter = ('category', )
+    list_display = ('__str__', 'title', 'search_tags', 'Description', 'created_at')
+    list_filter = ('category', 'search_tags')
+    form = ProductAdminForm
     inlines = [
         CountryCurrencyInLine
     ]
+
+    def Description(self, obj):
+        return format_html(obj.description)
 
 
 admin.site.register(Product, ProductAdmin)
