ecdcf4d80f596c748245a9643b8508e05d75f7e1
satya7289 <satyaprakashaman60@gmail.com>
2021-04-16 22:21:33 +0530

    coupon added

--- coupon/admin.py
+++ coupon/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from .models import Coupon
 
 # Register your models here.
-admin.site.register(Coupon)
+class CouponAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'coupon_name', 'coupon_code', 'coupon_method', 'coupon_value', 'coupon_usage_limit', 'coupon_used', 'coupon_category', 'coupon_valid', 'created_at')
+    list_filter = ('coupon_valid', )
+    search_fields = ('coupon_name',)
+
+
+admin.site.register(Coupon, CouponAdmin)
 admin.site.site_header = 'Punam Flutes Admin Console'
 admin.site.site_title = 'Punam Flutes Admin'
--- coupon/models.py
+++ coupon/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 
+from category.models import Category
+from commons.models import TimeStampedModel
+
 # Create your models here.
+METHOD_CHOICES = (
+        ('percent', 'percent'),
+        ('fixed_amount', 'Fixed Amout'),
+    )
 
-
-class Coupon(models.Model):
+class Coupon(TimeStampedModel):
     coupon_name = models.CharField(max_length=100)
     coupon_code = models.CharField(max_length=20)
-    coupon_value = models.IntegerField(default=0)
-    coupon_usage_limit = models.IntegerField(default=0)
-    coupon_category = models.ForeignKey(
-        'category.Category', on_delete=models.CASCADE, blank=True, null=True)
+    coupon_method= models.CharField(choices=METHOD_CHOICES, default='percent', max_length=20)
+    coupon_value = models.PositiveIntegerField(default=0)
+    coupon_usage_limit = models.PositiveIntegerField(default=0)
+    coupon_used = models.PositiveIntegerField(default=0)
+    coupon_category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
+    coupon_valid = models.BooleanField(default=True)
 
     def __str__(self):
         return self.coupon_name
