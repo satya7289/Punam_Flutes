ca1b5b6bdd01eacd0a4ac6982dd952755061ad81
satya7289 <satyaprakashaman60@gmail.com>
2021-04-23 11:59:59 +0530

    fix

--- product/admin.py
+++ product/admin.py
@@ -114,8 +114,8 @@ class InventoryAdmin(admin.ModelAdmin):
 
 class CountryCurrencyRateAdmin(admin.ModelAdmin):
     change_list_template = 'CountryCurrencyRate/change_list.html'
-    search_fields = ('country', 'currency', 'currency_symbol', 'currency_code', 'alpha_2_code', 'alpha_3_code')
-    list_display = ('__str__', 'country', 'alpha_2_code', 'alpha_3_code', 'numeric_code', 'currency', 'currency_code', 'currency_symbol', 'currency_rate', 'base', 'update_at')
+    search_fields = ('country', 'currency_symbol', 'currency_code', 'alpha_2_code', 'alpha_3_code')
+    list_display = ('__str__', 'country', 'alpha_2_code', 'alpha_3_code', 'numeric_code', 'currency_code', 'currency_symbol', 'currency_rate', 'base', 'update_at')
 
 
 admin.site.register(Product, ProductAdmin)
