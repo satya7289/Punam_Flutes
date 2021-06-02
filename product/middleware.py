3679e0d72e4179521ab474f2d6745d31f959a508
satya7289 <satyaprakashaman60@gmail.com>
2021-06-02 19:28:04 +0530

    update for ipstack

--- product/middleware.py
+++ product/middleware.py
@@ -10,10 +10,11 @@ class ProductPricingMiddleware:
         # Code to be executed for each request before
         # the view (and later middleware) are called.
 
-        restrict_for_country_data = ['/flutes_admin/', '/blogs/', '/testinomials/']
+        restrict_for_country_data = ['flutes_admin', 'blogs', 'testinomials', 'service-worker.js']
         # restrict_for_country_data_2 = ['/store', '/support', '/customer']
         # print(request.get_full_path())
-        if request.get_full_path() not in restrict_for_country_data:
+        requested_url = request.get_full_path().split('/')
+        if len(requested_url) > 1 and requested_url[1] not in restrict_for_country_data:
             # set the country to setting based on IP
             set_country_data(request)
 
