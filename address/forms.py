ae4f73973c5479b26cca439f00874c0319c968e7
satya7289 <satyaprakashaman60@gmail.com>
2021-06-09 16:27:04 +0530

    update address country

--- address/forms.py
+++ address/forms.py
@@ -1,12 +1,16 @@
+from os import stat
 from django import forms
 from django.forms import TextInput
 
 from commons.country_currency import country as COUNTRY
 from address.models import Address
-
+from product.models import CountryCurrencyRate
 
 class AddressCreateForm(forms.ModelForm):
-    country = forms.ChoiceField(choices=COUNTRY[1:], required=True)
+    countries = CountryCurrencyRate.objects.all().values('country').distinct()
+    countries_choice = tuple(map(lambda x: (x['country'], x['country']), countries))
+    countries_choice = (('', '-----'),) + countries_choice
+    country = forms.ChoiceField(choices=countries_choice, required=True)
     default = forms.BooleanField(required=False, initial=True)
 
     def __init__(self, *args, **kwargs):
@@ -16,6 +20,18 @@ class AddressCreateForm(forms.ModelForm):
             self.fields[field].required = True
         for field in self.Meta.not_required:
             self.fields[field].required = False
+    
+    # def save(self, *args, **kwargs): 
+    #     form_ = super().save()
+    #     req = kwargs.get('req')
+    #     if req:
+    #         state = req
+    #         print(req, req.get('state'), state, len(state), "satyuayfghj")
+    #         if len(state) == 2 and state[0] != '':
+    #             form_.state = state[0]
+    #             form_.save()
+    #         print(args, kwargs)
+    #     return form_
 
     class Meta:
         model = Address
